fec68ed62d80e101bc8042d24602f583c4699a28
Philippe Gagne <philgagne.pg@gmail.com>
2020-09-25 11:35:37 -0400

    Version 1.0 du projet #1.

--- poutre_rect.py
+++ poutre_rect.py
@@ -6,11 +6,16 @@ L = 100  # en mm
 b = 10  # en mm
 h = 20  # en mm
 
+# Conversion de GPa en N/mm**2
+
+E = E * 10 ** 3
+
 # Calcul de l'inertie
 
-I = 
+I = (b * h ** 3) / 12
 
 # Calcul de la déformation maximale
+formule_delta_max = (F * L ** 3) / ( 3 * E * I)
+delta_max = round(formule_delta_max,2)
 
-delta_max = 
-
+print(f"La déformation maximale de la poutre est {delta_max} mm")
--- poutre_section.py
+++ poutre_section.py
@@ -1,22 +1,64 @@
+import math
+
 # Initialisation des variables
 F = 10000  # en N
 E = 210  # en GPa = 10^3 N/mm^2
 L = 100  # en mm
 
+#Conversion de GPa en N/mm**2
+
+E = E * 10 ** 3
+
 # poutre rectangulaire
 b = 10  # en mm
 h = 20  # en mm
+I_rectangulaire = (b * h ** 3) / 12
 
 # poutre carrée
+
 a = 15  # en mm
+I_carree = a ** 4 / 12
 
 # poutre ronde
 d = 5  # en mm
+I_ronde = math.pi * d ** 4 / 64
 
 # poutre creuse
 D = 15  # en mm
 d = 5  # en mm
+I_creuse = (math.pi * (D ** 4 - d ** 4)) / 64
 
+# liste des types et valeurs de I pour chaque type
+liste_type_and_I = [
+    ("carree", I_carree),
+    ("rectangulaire", I_rectangulaire),
+    ("creuse", I_creuse),
+    ("ronde", I_ronde)
+]
+
+# Numero des index pour la liste des types et des valeurs de I
+INDEX_TYPE = 0
+INDEX_I = 1
 
 # Calcul de la section optimale
+liste_delta=[]
+
+for I in liste_type_and_I:
+    formule_delta_max = (F * L ** 3) / (3 * E * I[INDEX_I])
+    delta_max = round(formule_delta_max,2)
+    liste_delta.append(delta_max)
+
+# Trouver la valeur la plus petite de delta_max
+delta_optimale = min(liste_delta)
+
+# Trouver type optimal asssocie a la valeur optimale
+type_optimal = liste_type_and_I[liste_delta.index(delta_optimale)]
+
+# Afficher la valeur et le type optimaux
+print(f'Le type de section minimisant la déformation maximale '
+      f'est {type_optimal[INDEX_TYPE]}, avec une déformation de '
+      f'{delta_optimale} mm')
+
+
+
 
